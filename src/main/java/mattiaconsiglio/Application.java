b5732f9d59663cfbbdae6ec66a1a58f83f0f84e2
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-03-22 10:24:35 +0100

    Configured connection to db

--- src/main/java/mattiaconsiglio/Application.java
+++ src/main/java/mattiaconsiglio/Application.java
@@ -1,8 +1,14 @@
 package mattiaconsiglio;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("library");
 
     public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
         System.out.println("Hello World!");
     }
 }
